94b9d884f26aa53cd3f03fd76f2b33b513b1e68e
mokadde <mokaddes.ru2000@gmail.com>
2024-10-20 10:59:31 +0600

    commit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,13 @@
 
-import AppRoute from './Routes/AppRoute'
+import AppRoute from './Routes/AppRoute';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
     <AppRoute />
+    <ToastContainer position="top-center" />
     </>
   )
 }
